37f754f8aa9efc32de174ea7d829c5ffa295efd1
tillarnold <throwable42@gmail.com>
2015-04-18 10:49:18 +0200

    Use class syntax for DirtyGrid

--- src-es6/lib/dirtyGrid.js
+++ src-es6/lib/dirtyGrid.js
@@ -1,39 +1,39 @@
-let Fixed2dArray = require('fixed-2d-array')
+import Fixed2dArray from 'fixed-2d-array'
 
-let DirtyGrid = function(w, h, d) {
-  this._w = w
-  this._h = h
-  this._grid = new Fixed2dArray(w, h, d)
-  this._dirtyCoordinates = []
+
+export default class DirtyGrid {
+  constructor (w, h, d) {
+    this._w = w
+    this._h = h
+    this._grid = new Fixed2dArray(w, h, d)
+    this._dirtyCoordinates = []
+  }
+
+  get(x, y) {
+    return this._grid.get(x, y)
+  }
+
+  set(x, y, v) {
+    this._dirtyCoordinates.push({ x: x
+                                , y: y
+                                })
+
+    return this._grid.set(x, y, v)
+  }
+
+  getDirtyCoordinates() {
+    return this._dirtyCoordinates
+  }
+
+  cleanDirtyCoordinates() {
+    this._dirtyCoordinates = []
+  }
+
+  getHeight() {
+    return this._h
+  }
+
+  getWidth() {
+    return this._w
+  }
 }
-
-DirtyGrid.prototype.get = function(x, y) {
-  return this._grid.get(x, y)
-}
-
-DirtyGrid.prototype.set = function(x, y, v) {
-  this._dirtyCoordinates.push({ x: x
-                              , y: y
-                              })
-
-  return this._grid.set(x, y, v)
-}
-
-DirtyGrid.prototype.getDirtyCoordinates = function() {
-  return this._dirtyCoordinates
-}
-
-DirtyGrid.prototype.cleanDirtyCoordinates = function() {
-  this._dirtyCoordinates = []
-}
-
-DirtyGrid.prototype.getHeight = function() {
-  return this._h
-}
-
-DirtyGrid.prototype.getWidth = function() {
-  return this._w
-}
-
-
-module.exports = DirtyGrid
